090df3eceafe4a3d491ce5d1d85dada35200a108
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2020-11-27 20:01:16 +0300

    Create initDB.sql

---
+++ target/classes/db/initDB.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS reader;
+
+CREATE TABLE reader (
+  id VARCHAR(10) NOT NULL UNIQUE,
+  name VARCHAR(255) NOT NULL,
+  role VARCHAR(50) NOT NULL,
+  login VARCHAR(20) NOT NULL,
+  password VARCHAR(20) CHECK(Length(password)>7 and Length(password)<17),
+  PRIMARY KEY (login)
+);
+
+CREATE SEQUENCE book_bcid;
+
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE book (
+  bcid      INTEGER NOT NULL DEFAULT NEXTVAL('book_bcid'),
+  author    VARCHAR(255) NOT NULL,
+  title     VARCHAR(255) NOT NULL,
+  access INT CHECK( access>1 and access<5 and NOT NULL),
+  status INT CHECK(status<2 and NOT NULL),
+  reader VARCHAR(255) NULL,
+  country VARCHAR(128) NOT NULL,
+  city VARCHAR(128) NOT NULL,
+  street VARCHAR(128) NOT NULL,
+  house VARCHAR(5) NULL,
+  PRIMARY KEY (bcid),
+  FOREIGN KEY (reader) REFERENCES reader(Id)
+);
+
+DROP TABLE IF EXISTS order_board;
+
+CREATE TABLE order_board (
+  Author VARCHAR(255) NOT NULL,
+  Title VARCHAR(255) NOT NULL
+);
+
