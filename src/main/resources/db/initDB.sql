e2749806df851485b03a1b4d67ee076aaace238c
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2020-11-27 09:55:06 +0300

    Update initDB.sql

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,11 +1,12 @@
 DROP TABLE IF EXISTS reader;
 
 CREATE TABLE reader (
-  Id VARCHAR(10) NOT NULL UNIQUE,
-  Name VARCHAR(255) NOT NULL,
-  Login VARCHAR(20) NOT NULL,
-  Password VARCHAR(20) CHECK(Length(Password)>7 and Length(Password)<17),
-  PRIMARY KEY (Login)
+  id VARCHAR(10) NOT NULL UNIQUE,
+  name VARCHAR(255) NOT NULL,
+  role VARCHAR(50) NOT NULL,
+  login VARCHAR(20) NOT NULL,
+  password VARCHAR(20) CHECK(Length(password)>7 and Length(password)<17),
+  PRIMARY KEY (login)
 );
 
 DROP TABLE IF EXISTS book;
@@ -14,15 +15,15 @@ CREATE TABLE book (
   bcid      INTEGER NOT NULL AUTO_INCREMENT,
   author    VARCHAR(255) NOT NULL,
   title     VARCHAR(255) NOT NULL,
-  access INT CHECK( Access>1 and Access<5 and NOT NULL),
-  status INT CHECK(Status<2 and NOT NULL),
+  access INT CHECK( access>1 and access<5 and NOT NULL),
+  status INT CHECK(status<2 and NOT NULL),
   reader VARCHAR(255) NULL,
   country VARCHAR(128) NOT NULL,
   city VARCHAR(128) NOT NULL,
   street VARCHAR(128) NOT NULL,
   house VARCHAR(5) NULL,
   PRIMARY KEY (bcid),
-  FOREIGN KEY (Reader) REFERENCES reader(Id)
+  FOREIGN KEY (reader) REFERENCES reader(Id)
 );
 
 DROP TABLE IF EXISTS order_board;
@@ -31,3 +32,4 @@ CREATE TABLE order_board (
   Author VARCHAR(255) NOT NULL,
   Title VARCHAR(255) NOT NULL
 );
+
