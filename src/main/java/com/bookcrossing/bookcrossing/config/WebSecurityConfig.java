f6a4e3ef3d06e0ead7ded72f04991d6303f60989
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2020-11-27 09:53:30 +0300

    Create WebSecurityConfig.java

---
+++ src/main/java/com/bookcrossing/bookcrossing/config/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookcrossing.bookcrossing.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private javax.sql.DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select login, password, 'true' from reader "
+                        + "where login=?")
+                .authoritiesByUsernameQuery(
+                        "select login, role from reader "
+                        + "where login=?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                    .disable()
+                .authorizeRequests()
+                    .antMatchers("/","/registration").permitAll()                    
+                //Все остальные страницы требуют аутентификации
+                .anyRequest().authenticated()
+                .and()
+                    //Настройка для входа в систему
+                    .formLogin()
+                    .loginPage("/login")
+                    //Перенарпавление на главную страницу после успешного входа
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll()
+                    .logoutSuccessUrl("/");
+    }
+}
