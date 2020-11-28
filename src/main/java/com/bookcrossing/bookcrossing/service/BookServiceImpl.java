3326b14dab461eef2d12ff6f9aaed9bc6020d97a
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2020-11-29 00:51:17 +0300

    Update BookServiceImpl.java

--- src/main/java/com/bookcrossing/bookcrossing/service/BookServiceImpl.java
+++ src/main/java/com/bookcrossing/bookcrossing/service/BookServiceImpl.java
@@ -46,14 +46,14 @@ public class BookServiceImpl implements BookService {
         if (book.getBCID() == null) {
             book.setBCID(count()+1);
             KeyHolder keyHolder = new GeneratedKeyHolder();
-            namedParameterJdbcTemplate.update("INSERT INTO book (bcid, Author, Title, Access, Status, Reader, Country, City, Street, House) VALUES (:bcid, :author, :title, 2, 1, '0000000003', 'Беларусь', 'Брест', 'Гоголя', '48')",
+            namedParameterJdbcTemplate.update("INSERT INTO book (bcid, Author, Title,Access, Status, Reader, Country, City, Street, House) VALUES (5,'Грин Д.', 'Виноваты звёзды',2,0, '0000000003', 'Беларусь', 'Брест', 'Гоголя', '48')",
                     new MapSqlParameterSource()
                             .addValue("bcid", book.getBCID())
                             .addValue("author", book.getAuthor())
                             .addValue("title", book.getName())
                             .addValue("reader", book.getReader())
                             .addValue("access", book.getAccess())
-                            .addValue("status", book.getStatus())
+                            .addValue("status", book.getStatus()? 1 : 0)
                             .addValue("country", book.getCountry())
                             .addValue("city", book.getCity())
                             .addValue("street", book.getStreet())
