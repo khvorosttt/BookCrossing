58f3345b119be4dee9d4adb0420bad693e746cc7
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2020-11-29 01:17:45 +0300

    Update BookServiceImpl.java

--- src/main/java/com/bookcrossing/bookcrossing/service/BookServiceImpl.java
+++ src/main/java/com/bookcrossing/bookcrossing/service/BookServiceImpl.java
@@ -46,7 +46,7 @@ public class BookServiceImpl implements BookService {
         if (book.getBCID() == null) {
             book.setBCID(count()+1);
             KeyHolder keyHolder = new GeneratedKeyHolder();
-            namedParameterJdbcTemplate.update("INSERT INTO book (bcid, Author, Title,Access, Status, Reader, Country, City, Street, House) VALUES (:bcid, :author, :title,2,0, '0000000003', 'Беларусь', 'Брест', 'Гоголя', '48')",
+            namedParameterJdbcTemplate.update("INSERT INTO book (bcid, Author, Title,Access, Status, Reader, Country, City, Street, House) VALUES (:bcid, :author, :title,2,0, :reader, :country, :city, :street, :house)",
                     new MapSqlParameterSource()
                             .addValue("bcid", book.getBCID())
                             .addValue("author", book.getAuthor())
