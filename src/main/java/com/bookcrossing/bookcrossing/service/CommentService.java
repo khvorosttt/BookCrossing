03c235ed9098ae8ebec50907ae085e46b4d39406
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-10-07 22:19:45 +0300

    Create CommentService.java

---
+++ src/main/java/com/bookcrossing/bookcrossing/service/CommentService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookcrossing.bookcrossing.service;
+
+import com.bookcrossing.bookcrossing.domain.Book;
+import com.bookcrossing.bookcrossing.domain.Comment;
+import java.util.List;
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface CommentService {
+    
+    Comment save(Comment comment);
+    
+    List<Comment> findAll();
+    
+    List<Comment> findByBook(Book book);
+    
+    void delete(Comment comment);
+    
+    int count();
+}
