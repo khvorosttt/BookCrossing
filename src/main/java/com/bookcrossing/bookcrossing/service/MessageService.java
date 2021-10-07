90a286451dba58466d45a27bf775fbce631238bd
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-10-07 22:19:57 +0300

    Create MessageService.java

---
+++ src/main/java/com/bookcrossing/bookcrossing/service/MessageService.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookcrossing.bookcrossing.service;
+
+import com.bookcrossing.bookcrossing.domain.Message;
+import java.util.List;
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface MessageService {
+    
+    Message save(Message message);
+
+    List<Message> findAll();
+
+    List<Message> findBySender(Message message);
+    
+    List<Message> findBySenderRecipient(Message message);
+    
+    void delete(Message message);
+    
+    int count();
+}
