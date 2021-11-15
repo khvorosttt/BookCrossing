a3cfa7b779d877e34436a2f23309f8400432d502
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-11-16 01:06:45 +0300

    Update MessageController.java

--- src/main/java/com/bookcrossing/bookcrossing/controller/MessageController.java
+++ src/main/java/com/bookcrossing/bookcrossing/controller/MessageController.java
@@ -5,10 +5,33 @@
  */
 package com.bookcrossing.bookcrossing.controller;
 
+import com.bookcrossing.bookcrossing.domain.Message;
+import com.bookcrossing.bookcrossing.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
 /**
  *
  * @author Lenovo
  */
+@Controller
 public class MessageController {
+    @Autowired
+    private MessageService messageService;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
     
+    @MessageMapping("/chat")
+    public void processMessage(@Payload Message message) {
+        String chatId=message.getId_sender()+"_"+message.getId_recipient();
+        message.setChatId(chatId);
+
+        Message saved = messageService.save(message);
+        
+        messagingTemplate.convertAndSendToUser(
+                message.getId_recipient(),"/queue/messages",message);
+    }
 }
