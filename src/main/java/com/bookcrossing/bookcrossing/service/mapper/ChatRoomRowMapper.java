b94bdfb01d56281ffc5e094f94f2d1f5e7459800
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-11-16 22:49:34 +0300

    Create ChatRoomRowMapper.java

---
+++ src/main/java/com/bookcrossing/bookcrossing/service/mapper/ChatRoomRowMapper.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookcrossing.bookcrossing.service.mapper;
+
+import com.bookcrossing.bookcrossing.domain.ChatRoom;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Lenovo
+ */
+@Component
+public class ChatRoomRowMapper implements RowMapper<ChatRoom> {
+    @Override
+    public ChatRoom mapRow(ResultSet rs, int rowNum) throws SQLException {
+        ChatRoom chatRoom = new ChatRoom();
+        chatRoom.setId(rs.getInt("id"));
+        chatRoom.setChatId(rs.getString("chatId"));
+        chatRoom.setSenderId(rs.getString("senderId"));
+        chatRoom.setRecipientId(rs.getString("recipientId"));
+        return chatRoom;
+    }
+}
