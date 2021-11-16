7efa588af256f8154643732d8840a074a189711f
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-11-16 17:47:16 +0300

    Update MessageServiceImpl.java

--- src/main/java/com/bookcrossing/bookcrossing/service/MessageServiceImpl.java
+++ src/main/java/com/bookcrossing/bookcrossing/service/MessageServiceImpl.java
@@ -30,7 +30,7 @@ public class MessageServiceImpl implements MessageService {
         this.messageRowMapper = messageRowMapper;
     }
     
-    @Override
+    /*@Override
     public Message save(Message message) {
         if (message.getId_message() == null) {
             message.setId_message(count()+1);
@@ -58,7 +58,7 @@ public class MessageServiceImpl implements MessageService {
                             .addValue("is_read", message.getIs_read()));
         }
         return message;
-    }
+    }*/
 
     @Override
     public List<Message> findAll() {
