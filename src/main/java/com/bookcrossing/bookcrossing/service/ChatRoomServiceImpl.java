6cad3e70025704332817674043afaac530683b87
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-11-16 23:36:35 +0300

    Update ChatRoomServiceImpl.java

--- src/main/java/com/bookcrossing/bookcrossing/service/ChatRoomServiceImpl.java
+++ src/main/java/com/bookcrossing/bookcrossing/service/ChatRoomServiceImpl.java
@@ -12,11 +12,13 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Lenovo
  */
+@Service
 public class ChatRoomServiceImpl implements ChatRoomService{
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
