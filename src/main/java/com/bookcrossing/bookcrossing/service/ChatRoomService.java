3a756de1e8f490613a501e1c5d1be3eca060ea4e
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-11-16 23:27:30 +0300

    Create ChatRoomService.java

---
+++ src/main/java/com/bookcrossing/bookcrossing/service/ChatRoomService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bookcrossing.bookcrossing.service;
+
+import com.bookcrossing.bookcrossing.domain.ChatRoom;
+import java.util.List;
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface ChatRoomService {
+    
+    List<ChatRoom> findByChatId(String senderId, String recipientId);
+    
+    List<ChatRoom> findBySenderId(String senderId);
+    
+    List<ChatRoom> findAll();
+    int count();
+}
