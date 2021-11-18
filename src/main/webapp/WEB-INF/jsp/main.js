d37e38d2b8dda7e670409367e0a98d62ea970712
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-11-18 16:37:51 +0300

    Update main.js

--- src/main/webapp/WEB-INF/jsp/main.js
+++ src/main/webapp/WEB-INF/jsp/main.js
@@ -18,17 +18,17 @@ var colors = [
 connect(true);
 
 function connect(event) {
-    //username = document.querySelector('#name').value.trim();
+    username = document.querySelector('#name').value.trim();
 
-    //if(username) {
-     //   usernamePage.classList.add('hidden');
+    if(username) {
+        usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
         var socket = new SockJS('/ws');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
-    //}
+    }
     event.preventDefault();
 }
 
