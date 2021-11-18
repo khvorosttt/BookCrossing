dc91ff880143e3a7befef303cd825aa637e1ec73
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-11-18 18:46:13 +0300

    Update main.js

--- src/main/webapp/WEB-INF/jsp/main.js
+++ src/main/webapp/WEB-INF/jsp/main.js
@@ -15,7 +15,6 @@ var colors = [
     '#2196F3', '#32c787', '#00BCD4', '#ff5652',
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
 ];
-connect(true);
 function connect(event) {
     username = document.querySelector('#name').value.trim();
 
