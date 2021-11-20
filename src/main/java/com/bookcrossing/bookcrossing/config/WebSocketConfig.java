11fcc5158369f92f6adab6862e98e33eee0ee25d
khvorosttt <57453069+khvorosttt@users.noreply.github.com>
2021-11-20 19:08:38 +0300

    Update WebSocketConfig.java

--- src/main/java/com/bookcrossing/bookcrossing/config/WebSocketConfig.java
+++ src/main/java/com/bookcrossing/bookcrossing/config/WebSocketConfig.java
@@ -56,7 +56,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
-        //registry.enableSimpleBroker("/messages");
+        //registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/messages");
     }
 }
